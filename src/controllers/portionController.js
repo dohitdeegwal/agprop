ba8d455283647784f0cdc6983cef7a5c4ca9b1cb
dohitdeegwal <76277872+dohitdeegwal@users.noreply.github.com>
2023-12-17 16:23:29 +0530

    add functionality to portions

--- src/controllers/portionController.js
+++ src/controllers/portionController.js
@@ -80,6 +80,96 @@ const portionController = {
             res.status(500).json({ error: 'Internal Server Error' });
         }
     },
+
+    async updatePortion(req, res) {
+        try {
+            const { propertyId, landlordId, brokerId, ...portionData } = req.body;
+
+            let property, landlord, broker;
+
+            // Check if the landlord exists
+            landlord = await Landlord.findById(landlordId);
+            if (landlordId && !landlord) {
+                return res.status(404).json({ error: 'Landlord not found' });
+            }
+
+            // Check if the broker exists
+            broker = await Broker.findById(brokerId);
+            if (brokerId && !broker) {
+                return res.status(404).json({ error: 'Broker not found' });
+            }
+
+            // Check if the property exists
+            if (propertyId) {
+                property = await Property.findById(propertyId);
+                if (propertyId && !property) {
+                    console.log('Property not found');
+                    return res.status(404).json({ error: 'Property not found' });
+                }
+            } else {
+                // Create a new property if propertyId is not provided
+                property = await Property.create({name: portionData.propertyName});
+            }
+
+            // Update the portion and associate it with the property, landlord, and broker
+            const portion = await Portion.findByIdAndUpdate(
+                req.params.id,
+                {
+                    ...portionData,
+                    property: property._id,
+                    landlord: landlord._id,
+                    broker: broker._id,
+                },
+                { new: true }
+            );
+
+            property.portions.push(portion._id);
+            await property.save();
+
+            res.json(portion);
+        } catch (error) {
+            if (error.name === 'ValidationError') {
+                // Mongoose validation error
+                const validationErrors = Object.values(error.errors).map(err => err.message);
+                res.status(400).json({ error: 'Validation Error', messages: validationErrors });
+            } else {
+                console.error(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        }
+    },
+
+    async deletePortion(req, res) {
+        const { id } = req.params;
+
+        if (!isValidObjectId(id)) {
+            return res.status(400).json({ error: 'Invalid ID format' });
+        }
+
+        try {
+            const portion = await Portion.findById(id);
+            if (!portion) {
+                return res.status(404).json({ error: 'Portion not found' });
+            }
+
+            // Remove the portion from the associated property
+            const property = await Property.findById(portion.property);
+            if (property) {
+                property.portions = property.portions.filter(
+                    (portionId) => portionId.toString() !== id
+                );
+                await property.save();
+            }
+
+            // Delete the portion
+            await Portion.findByIdAndDelete(id);
+
+            res.json(portion);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    },
 };
 
 module.exports = portionController;
