d7dfd7edb7bf44b361f057f31c75ae996fb69866
dohitdeegwal <76277872+dohitdeegwal@users.noreply.github.com>
2023-12-17 16:18:48 +0530

    Add validations to landlord controller

--- src/controllers/landlordController.js
+++ src/controllers/landlordController.js
@@ -1,4 +1,7 @@
+const mongoose = require('mongoose');
 const Landlord = require('../models/Landlord');
+
+const isValidObjectId = mongoose.isValidObjectId;
 
 const landlordController = {
     async createLandlord(req, res) {
@@ -6,8 +9,17 @@ const landlordController = {
             const landlord = await Landlord.create(req.body);
             res.status(201).json(landlord);
         } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal Server Error' });
+            if (error.name === 'ValidationError') {
+                // Handle Mongoose validation errors
+                const errors = {};
+                Object.keys(error.errors).forEach((key) => {
+                    errors[key] = error.errors[key].message;
+                });
+                res.status(400).json({ errors });
+            } else {
+                console.error(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
         }
     },
 
@@ -22,9 +34,19 @@ const landlordController = {
     },
 
     async getLandlordById(req, res) {
+        const { id } = req.params;
+
+        if (!isValidObjectId(id)) {
+            return res.status(400).json({ error: 'Invalid ID format' });
+        }
+
         try {
-            const landlord = await Landlord.findById(req.params.id);
-            res.json(landlord);
+            const landlord = await Landlord.findById(id);
+            if (!landlord) {
+                res.status(404).json({ error: 'Landlord not found' });
+            } else {
+                res.json(landlord);
+            }
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Internal Server Error' });
@@ -32,23 +54,52 @@ const landlordController = {
     },
 
     async updateLandlord(req, res) {
+        const { id } = req.params;
+
+        if (!isValidObjectId(id)) {
+            return res.status(400).json({ error: 'Invalid ID format' });
+        }
+
         try {
             const landlord = await Landlord.findByIdAndUpdate(
-                req.params.id,
+                id,
                 req.body,
                 { new: true }
             );
-            res.json(landlord);
+            if (!landlord) {
+                res.status(404).json({ error: 'Landlord not found' });
+            } else {
+                res.json(landlord);
+            }
         } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal Server Error' });
+            if (error.name === 'ValidationError') {
+                // Handle Mongoose validation errors
+                const errors = {};
+                Object.keys(error.errors).forEach((key) => {
+                    errors[key] = error.errors[key].message;
+                });
+                res.status(400).json({ errors });
+            } else {
+                console.error(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
         }
     },
 
     async deleteLandlord(req, res) {
+        const { id } = req.params;
+
+        if (!isValidObjectId(id)) {
+            return res.status(400).json({ error: 'Invalid ID format' });
+        }
+
         try {
-            const landlord = await Landlord.findByIdAndDelete(req.params.id);
-            res.json(landlord);
+            const landlord = await Landlord.findByIdAndDelete(id);
+            if (!landlord) {
+                res.status(404).json({ error: 'Landlord not found' });
+            } else {
+                res.json(landlord);
+            }
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Internal Server Error' });
